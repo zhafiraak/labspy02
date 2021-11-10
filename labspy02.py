7855e194b74174541f6d42fd11b6cf899fd561c8
zhafiraak <zhafirakhafianti19@gmail.com>
2021-11-10 10:13:09 +0700

    py2

---
+++ labspy02.py
@@ -0,0 +1,13 @@
+print("Program mencari Bilangan Terbesar")
+
+
+a = int(input("Masukan bilangan 1: "))
+b = int(input("Masukan bilangan 2: "))
+c = int(input("Masukan bilangan 3: "))
+
+if a > b and a > c:
+      print ("Bilangan 1 terbesar")
+elif b > a and b > c:
+      print ("Bilangan 2 terbesar")
+else:
+      print("Bilangan 3 terbesar") 
